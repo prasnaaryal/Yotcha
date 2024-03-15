3d5ebbf0d2c59b84cb87259cb61ea1a48706176e
prasnaaryal <prasna101@gmail.com>
2024-03-15 22:43:20 +0545

    created header

--- src/layouts/components/Topbar.tsx
+++ src/layouts/components/Topbar.tsx
@@ -1,30 +1,76 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const Topbar = () => {
+  const pages = [
+    {
+      title: "Buy",
+      url: "/buy",
+    },
+
+    {
+      title: "Sale",
+      url: "/sale",
+    },
+
+    {
+      title: "Rent",
+      url: "/rent",
+    },
+
+    {
+      title: "New Projects",
+      url: "/new-projects",
+    },
+
+    {
+      title: "Mortgages",
+      url: "/mortgage",
+    },
+  ];
+
   return (
-    <div>
-      <div className='flex items-center'><img src="./assets/Vector.png" alt=""/><p>Yotcha</p> </div>
-      <div>
-        <nav >
-          <Link href={"#"}>Buy</Link>
-          <Link href={"#"}>Sale</Link>
-          <Link href={"#"}>Rent</Link>
-          <Link href={"#"}>New Projects</Link>
-          <Link href={"#"}>Mortgages</Link>
-        </nav>
-         </div>
-      <div>
+    <header className="container flex w-full justify-between py-5">
+      <div className="flex-1  flex items-center w-full gap-14">
+        <Image src="/assets/logo1.svg" width={95} height={33} alt="logo" />
 
-      <Button>
-  <Link href="#">Login</Link>
-</Button>
+        <NavigationMenu>
+          <NavigationMenuList className="flex gap-7">
+            {pages &&
+              pages.map((page, index) => (
+                <NavigationMenuItem key={index}>
+                  <Link href={page?.url} legacyBehavior passHref>
+                    <NavigationMenuLink>{page?.title}</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
 
-        
-         </div>
-    </div>
-  )
-}
+      <div className="flex-1 flex w-full justify-end gap-4">
+        <Button variant={"default"} size={"lg"}>
+          Add listing
+        </Button>
 
-export default Topbar
+        <Button variant={"outline"} size={"lg"}>
+          Login
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default Topbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,7 @@ const config = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#00C5B4",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
