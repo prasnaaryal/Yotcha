964d0fe409647742c8043e7e1c3c7f6b08e58e62
prasnaaryal <prasna101@gmail.com>
2024-03-21 10:33:45 +0545

    landing updated

--- src/views/Landing/Landing.tsx
+++ src/views/Landing/Landing.tsx
@@ -1,22 +1,57 @@
-
-import Image from 'next/image'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React from "react";
 
 const Landing = () => {
   return (
     <div>
-       {/* Background image container */}
-       <div className="">
-        <Image
+      {/* Background image container */}
+      <div>
+        <img
           src="/assets/Rectangle.png"
-          height={695}
-          width={1440}
-          alt="Background"
+          className="w-[100vw] h-[100vh] object-cover"
+          alt="image1"
         />
       </div>
 
-    </div>
-  )
-}
+      <div className="absolute inset-0 flex items-center justify-center flex-col">
+        <h1 className="text-white text-6xl font-bold">Find Your Dream Home</h1>
+        <p className="text-2xl text-white mt-4	">
+          We Have Over Million Properties For You.
+        </p>
 
-export default Landing
+        <div className="flex gap-2.5 mt-10">
+          <Button variant="default" size="lg">
+            Buy
+          </Button>
+
+          <Button variant="outline" size="lg">
+            Rent
+          </Button>
+
+          <Button variant="outline" size="lg">
+            Sell
+          </Button>
+        </div>
+
+        {/* Box behind the search bar */}
+        <div className="bg-white bg-opacity-20  p-2 rounded-md w-xl mt-6 xl:w-[790px]">
+          <div className="bg-white p-4 rounded-md w-full">
+            <div className="flex items-center space-x-2">
+              <Input type="email" placeholder="Search for a Property" />
+              <Button type="submit">Search</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute inset-0 flex items-center justify-center flex-col">
+        
+        
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
